ef767beb147f1a4d9f3fa38dcd0e9d96083c88e6
Vayun-Goel <120119682+Vayun-Goel@users.noreply.github.com>
2024-09-05 15:50:43 +0530

    Update hyper_param_tuner.py

--- Stage-1_coding/hyper_param_tuner.py
+++ Stage-1_coding/hyper_param_tuner.py
@@ -2,24 +2,37 @@ from ultralytics import YOLO
 
 from IPython.display import display, Image
 
-from roboflow import Roboflow
-
-rf = Roboflow(api_key="YLXTeUbudCvbIf1n2CSf")
-project = rf.workspace("vayun-goel-jlreu").project("pipeline1-boundingboxweld")
-version = project.version(2)
-dataset = version.download("yolov8-obb")
-
+import requests
+import zipfile
+import io
+import os
 import yaml
 
-with open(f'{dataset.location}/data.yaml', 'r') as file:
+# URL of the dataset
+url = "https://app.roboflow.com/ds/XIoAKveJkT?key=8JJBNShNQK"
+
+# Download the zip file
+response = requests.get(url)
+zip_file = zipfile.ZipFile(io.BytesIO(response.content))
+
+# Create a directory to extract the files
+dataset_dir = "downloaded_dataset"
+os.makedirs(dataset_dir, exist_ok=True)
+
+# Extract all files
+zip_file.extractall(dataset_dir)
+
+# Update the data.yaml file
+yaml_path = os.path.join(dataset_dir, 'data.yaml')
+with open(yaml_path, 'r') as file:
     data = yaml.safe_load(file)
 
-data['path'] = dataset.location
+data['path'] = os.path.abspath(dataset_dir)
 
-with open(f'{dataset.location}/data.yaml', 'w') as file:
+with open(yaml_path, 'w') as file:
     yaml.dump(data, file, sort_keys=False)
 
 model = YOLO("yolov8n-obb.pt")
 
 # Tune hyperparameters on COCO8 for 30 epochs
-model.tune(data=f"{dataset.location}/data.yaml", epochs=100, iterations=100, optimizer="AdamW", plots=False, save=False, val=False)
+model.tune(data=yaml_path, epochs=100, iterations=100, optimizer="AdamW", plots=False, save=False, val=False)
